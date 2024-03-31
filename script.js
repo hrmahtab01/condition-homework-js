4b5a4e8cd9c02618708de3f875244419bbd1ba04
mahtab <161712272+hrmahtab01@users.noreply.github.com>
2024-03-31 13:04:42 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+
+let A = prompt("apni ki cit student?")
+
+
+if(A == "yes"){
+    console.log("apni vitore dhukte paren")
+    
+}else if (A == "no" ) {
+    console.log("sorry age admision hon")
+}else
+   {
+    console.log("votore jete parben na")
+   }
+
+
+   let B = prompt("apni kon department er student?" , "mern")
+
+if ( B == "mern"){
+    console.log("apanr mento wasim sir")
+}else if( B =="other"){
+    console.log("porshu asun ")
+}
+else{
+    console.log("sorry apnar mentor ekhono thik hoyni")
+}
+
+let C = prompt("select your proffesion","student/job")
+
+if( C == "student"){
+    console.log("apnake dine somoy dite hobe 10 hour")
+}else if( C == "job"){
+    console.log("apnake dine somoy dite hobe 5 hour")
+}else if( C == "other"){
+    console.log("apnake dine somoy dite hobe 18 hour")
+}
+else{
+    console.log("apni vul information diyechen")
+}
